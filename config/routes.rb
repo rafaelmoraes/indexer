4f489462b85d615d6846007c7d2e4b4f996bd8b7
Rafael Moraes <roliveira.moraes@gmail.com>
2019-04-03 20:30:19 -0300

    Removes unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  jsonapi_resources :webpages
+  jsonapi_resources :webpages, only: %i[index create] do
+    jsonapi_resources :headers, only: :index
+  end
 end
