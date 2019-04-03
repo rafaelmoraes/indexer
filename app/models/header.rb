9d505398321ebc0973334378fdb851631b31a996
Rafael Moraes <roliveira.moraes@gmail.com>
2019-04-03 16:46:05 -0300

    Changes Header#tag validation from validates_format_of to validates_inclusion_of

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Header < ApplicationRecord
-  TAGS_REGEX = /\Ah(1|2|3)\z/.freeze
+  TAGS = %w[h1 h2 h3].freeze
 
   belongs_to :webpage
 
   validates_presence_of :tag, :link
-  validates_format_of :tag,
-                      with: TAGS_REGEX,
-                      message: "must be h1 or h2 or h3"
+  validates_inclusion_of :tag,
+                      in: TAGS,
+                      message: "must be #{TAGS.join(' or ')}"
 end
